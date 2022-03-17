ee552cc13db53bbadcc9180275182ae10eb8fa0f
Pedro Paulo <pedropaulotj@gmail.com>
2022-03-17 09:54:11 -0300

    fix: Melhorias no carregamento de posts com dados faltantes

--- components/PostCard/PostCard.js
+++ components/PostCard/PostCard.js
@@ -22,15 +22,15 @@ export default function PostCard({postData, onCardButtonClick}) {
   return (
     <StyledCardContainer>
 
-    {postData.featured_media
-      ?
+    {/* {postData.featured_media
+      ? */}
         <Link href={{
         pathname: `/single/${postData.id}`,
         query: { slug: postData.slug }
         }} >
-          <a><StyledCardThumbnail src={postData.featured_media.medium} alt={postData.title} /></a>
+          <a><StyledCardThumbnail src={postData?.featured_media?.medium ? postData?.featured_media?.medium : `https://textoverimage.moesif.com/image?x_align=center&y_align=middle&margin=32&text=${postData.title}&image_url=https://img.freepik.com/fotos-gratis/plano-de-fundo-texturizado-cinza_24972-763.jpg?w=1380&t=st=1647520906~exp=1647521506~hmac=c8c53c315910a37ec554c3a56c70c8d129ce72f3bebc74242305d3909377db7b`} alt={postData.title} /></a>
         </Link>
-      : null}
+      {/* : null} */}
 
       <StyledCardDateCategory>
 
--- pages/single/[id].js
+++ pages/single/[id].js
@@ -35,16 +35,15 @@ import {
 import Head from "next/head";
 
 export async function getStaticPaths() {
-
   return {
     paths: [
       {
         params: {
-          id: '265206'
-        }
-      }
+          id: "265206",
+        },
+      },
     ],
-    fallback: 'blocking',
+    fallback: "blocking",
   };
 }
 
@@ -54,15 +53,15 @@ export async function getStaticProps(context) {
   // O que eu passar aqui, ele vai virar SEO
   // const response = await api.get(`posts/${id}`)
   const response = await api
-  .get(`posts/${id}`)
-  .then((response) => {
-    // console.log(response);
-    return response.data;
-  })
-  .catch((error) => {
-    console.log(error);
-    return false;
-  });
+    .get(`posts/${id}`)
+    .then((response) => {
+      // console.log(response);
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+      return false;
+    });
 
   // if (response.status === "404") {
   //   return { notFound: true };
@@ -98,7 +97,7 @@ export async function getStaticProps(context) {
 
 // }
 
-export default function Single({response}) {
+export default function Single({ response }) {
   // console.log(response);
 
   const router = useRouter();
@@ -112,29 +111,29 @@ export default function Single({response}) {
   // const [loading, setLoading] = useState(true);
 
   // useEffect(() => {
-    
-    // setPostData(response);
-    // setLoading(false);
 
-    // if(response.status !== '404'){
-    //   setLoading(false);
-    // }
+  // setPostData(response);
+  // setLoading(false);
 
-    // const getData = async () => {
-    //   // O que eu passar aqui, ele vai virar SEO
-    //   await api.get(`posts/${id}`)
-    //   .then((response) => {
-    //     console.log(response.data);
-    //     setPostData(response.data);
-    //     setLoading(false);
-    //     return;
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //     return false;
-    //   });
-    // }
-    // getData();
+  // if(response.status !== '404'){
+  //   setLoading(false);
+  // }
+
+  // const getData = async () => {
+  //   // O que eu passar aqui, ele vai virar SEO
+  //   await api.get(`posts/${id}`)
+  //   .then((response) => {
+  //     console.log(response.data);
+  //     setPostData(response.data);
+  //     setLoading(false);
+  //     return;
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //     return false;
+  //   });
+  // }
+  // getData();
   // }, []);
 
   return (
@@ -143,176 +142,182 @@ export default function Single({response}) {
 
       <Header onlyLogo />
 
-        <Head>
-          <title>{`${response.metas["title"]} | Translation, Inc`}</title>
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="38x38"
-            href="../images/favicon.png"
-          />
+      <Head>
+        <title>{`${response.metas["title"]} | Translation, Inc`}</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="38x38"
+          href="../images/favicon.png"
+        />
 
-          <meta name="ampUrl" content={response.metas["ampUrl"]} />
-          <meta
-            name="article:author"
-            content={response.metas["article:author"]}
-          />
-          <meta
-            name="article:publisher"
-            content={response.metas["article:publisher"]}
-          />
-          <meta
-            name="article:section"
-            content={response.metas["article:section"]}
-          />
-          {/* <meta name="article:tag" content={response.metas["article:tag"]} /> */}
-          <meta name="canonical" content={response.metas["canonical"]} />
-          <meta name="description" content={response.metas["description"]} />
-          <meta name="title" content={response.metas["title"]} />
-          <meta name="twitter:card" content={response.metas["twitter:card"]} />
-          <meta
-            name="twitter:creator"
-            content={response.metas["twitter:creator"]}
-          />
-          <meta name="twitter:site" content={response.metas["twitter:site"]} />
-          <meta
-            property="og:description"
-            content={response.metas["og:description"]}
-          />
-          <meta property="og:image" content={response.metas["og:image"]} />
-          <meta
-            property="og:image:alt"
-            content={response.metas["og:image:alt"]}
-          />
-          <meta
-            property="og:image: height"
-            content={response.metas["image: height"]}
-          />
-          <meta
-            property="og:image:secure_url"
-            content={response.metas["og:image:secure_url"]}
-          />
-          <meta
-            property="og:image: width"
-            content={response.metas["og:image: width"]}
-          />
-          <meta property="og:locale" content={response.metas["og:locale"]} />
-          <meta
-            property="og:site_name"
-            content={response.metas["og:site_name"]}
-          />
-          <meta
-            property="og:title"
-            content={`${response.metas["og:title"]} | Translation, Inc`}
-          />
-          <meta property="og:type" content={response.metas["og:type"]} />
-        </Head>
-        
-        <PostSingleGrid>
-          <PostContainer>
-            <PostTitle>{response.title}</PostTitle>
-            {response.headline ? (
-              <PostHeadline>{response.headline}</PostHeadline>
-            ) : null}
+        <meta name="ampUrl" content={response.metas["ampUrl"]} />
+        <meta
+          name="article:author"
+          content={response.metas["article:author"]}
+        />
+        <meta
+          name="article:publisher"
+          content={response.metas["article:publisher"]}
+        />
+        <meta
+          name="article:section"
+          content={response.metas["article:section"]}
+        />
+        {/* <meta name="article:tag" content={response.metas["article:tag"]} /> */}
+        <meta name="canonical" content={response.metas["canonical"]} />
+        <meta name="description" content={response.metas["description"]} />
+        <meta name="title" content={response.metas["title"]} />
+        <meta name="twitter:card" content={response.metas["twitter:card"]} />
+        <meta
+          name="twitter:creator"
+          content={response.metas["twitter:creator"]}
+        />
+        <meta name="twitter:site" content={response.metas["twitter:site"]} />
+        <meta
+          property="og:description"
+          content={response.metas["og:description"]}
+        />
+        <meta property="og:image" content={response.metas["og:image"]} />
+        <meta
+          property="og:image:alt"
+          content={response.metas["og:image:alt"]}
+        />
+        <meta
+          property="og:image: height"
+          content={response.metas["image: height"]}
+        />
+        <meta
+          property="og:image:secure_url"
+          content={response.metas["og:image:secure_url"]}
+        />
+        <meta
+          property="og:image: width"
+          content={response.metas["og:image: width"]}
+        />
+        <meta property="og:locale" content={response.metas["og:locale"]} />
+        <meta
+          property="og:site_name"
+          content={response.metas["og:site_name"]}
+        />
+        <meta
+          property="og:title"
+          content={`${response.metas["og:title"]} | Translation, Inc`}
+        />
+        <meta property="og:type" content={response.metas["og:type"]} />
+      </Head>
 
-            <PostTopInfoSection>
-              <PostDateSection>
-                <div style={{marginRight: '10px'}}>
-                  <PostDatePublished>
-                    {new Date(response.published).toLocaleDateString()}
-                  </PostDatePublished>
+      <PostSingleGrid>
+        <PostContainer>
+          <PostTitle>{response.title}</PostTitle>
+          {response.headline ? (
+            <PostHeadline>{response.headline}</PostHeadline>
+          ) : null}
 
-                  <PostDateModified>
-                    Modified {new Date(response.modified).toLocaleDateString()}
-                  </PostDateModified>
-                </div>
-                <div style={{display: 'flex', alignItems: 'center', marginRight: '10px'}}>
-                  <PostAuthorPicture
-                    src={
-                      response.author.picture !== ''
-                        ? response.author.picture
-                        : "https://beta.mejorconsalud.com/wp-content/themes/base-deploy/assets/img/about-us/no_profile.png"
-                    }
-                    alt="Author"
-                  />
-                  <PostAuthorName>{response.author.name}</PostAuthorName>
-                </div>
+          <PostTopInfoSection>
+            <PostDateSection>
+              <div style={{ marginRight: "10px" }}>
+                <PostDatePublished>
+                  {new Date(response.published).toLocaleDateString()}
+                </PostDatePublished>
 
-              </PostDateSection>
-              <div style={{marginTop: '10px', marginBottom: '-6px'}}>
-                <CategoryBullet data={response.categories} />
+                <PostDateModified>
+                  Modified {new Date(response.modified).toLocaleDateString()}
+                </PostDateModified>
               </div>
-            </PostTopInfoSection>
-
-            {response.featured_media["1536x1536"] !== null &&
-            response.featured_media["1536x1536"] !== undefined ? (
-              <PostThumbnail src={response.featured_media["1536x1536"]} alt="" />
-            ) : null}
-
-            <PostContent>{parse(response.content)}</PostContent>
-
-            <PostTagsWraper>
-              <PostTags>
-                Tags:
-              </PostTags>
-              {response.tags.length !== 0 ? (
-                  <CategoryBullet data={response.tags} />
-                ) : (
-                  ""
-                )}
-            </PostTagsWraper>
-
-            {response.bibliography ? (
-              <PostBibliography>
-                Bibliography: {parse(response.bibliography)}
-              </PostBibliography>
-            ) : null}
-          </PostContainer>
-
-          <PostAuthorProfileContainer>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "flex-start",
-                alignItems: "center",
-                flexDirection: "row",
-              }}
-            >
-              <PostAuthorProfilePicture
-                src={
-                  response.author.picture
-                    ? response.author.picture
-                    : "https://beta.mejorconsalud.com/wp-content/themes/base-deploy/assets/img/about-us/no_profile.png"
-                }
-                alt="Thumbnail"
-                alt="Thumbnail"
-              />
               <div
                 style={{
                   display: "flex",
-                  alignItems: "flex-start",
-                  flexDirection: "column",
+                  alignItems: "center",
+                  marginRight: "10px",
                 }}
               >
-                <PostAuthorProfileName>
-                  {response.author.name}
-                </PostAuthorProfileName>
-                <PostAuthorProfileJob>
-                  {response.author.profession}
-                </PostAuthorProfileJob>
+                <PostAuthorPicture
+                  src={
+                    response?.author?.picture
+                      ? response?.author?.picture
+                      : "https://beta.mejorconsalud.com/wp-content/themes/base-deploy/assets/img/about-us/no_profile.png"
+                  }
+                  alt="Author"
+                />
+                <PostAuthorName>{response?.author?.name}</PostAuthorName>
               </div>
+            </PostDateSection>
+            <div style={{ marginTop: "10px", marginBottom: "-6px" }}>
+              <CategoryBullet data={response.categories} />
             </div>
+          </PostTopInfoSection>
 
-            {response.author.description !== "" ? (
-              <PostAuthorProfileDescription>
-                {parse(response.author.description)}
-              </PostAuthorProfileDescription>
+          <PostThumbnail
+            src={
+              response?.featured_media !== null &&
+              response?.featured_media["1536x1536"] !== undefined
+                ? response?.featured_media["1536x1536"]
+                : `https://textoverimage.moesif.com/image?x_align=center&y_align=middle&margin=32&text=${response.title}&image_url=https://img.freepik.com/fotos-gratis/plano-de-fundo-texturizado-cinza_24972-763.jpg?w=1380&t=st=1647520906~exp=1647521506~hmac=c8c53c315910a37ec554c3a56c70c8d129ce72f3bebc74242305d3909377db7b`
+            }
+            alt=""
+          />
+
+          <PostContent>{parse(response.content)}</PostContent>
+
+          <PostTagsWraper>
+            <PostTags>Tags:</PostTags>
+            {response?.tags.length !== 0 ? (
+              <CategoryBullet data={response.tags} />
             ) : (
               ""
             )}
-          </PostAuthorProfileContainer>
-        </PostSingleGrid>
-        
+          </PostTagsWraper>
+
+          {response.bibliography ? (
+            <PostBibliography>
+              Bibliography: {parse(response.bibliography)}
+            </PostBibliography>
+          ) : null}
+        </PostContainer>
+
+        <PostAuthorProfileContainer>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-start",
+              alignItems: "center",
+              flexDirection: "row",
+            }}
+          >
+            <PostAuthorProfilePicture
+              src={
+                response?.author?.picture
+                  ? response?.author?.picture
+                  : "https://beta.mejorconsalud.com/wp-content/themes/base-deploy/assets/img/about-us/no_profile.png"
+              }
+              alt="Thumbnail"
+            />
+            <div
+              style={{
+                display: "flex",
+                alignItems: "flex-start",
+                flexDirection: "column",
+              }}
+            >
+              <PostAuthorProfileName>
+                {response?.author?.name}
+              </PostAuthorProfileName>
+              <PostAuthorProfileJob>
+                {response?.author?.profession}
+              </PostAuthorProfileJob>
+            </div>
+          </div>
+
+          {response?.author?.description !== "" ? (
+            <PostAuthorProfileDescription>
+              {parse(response?.author?.description ?? "")}
+            </PostAuthorProfileDescription>
+          ) : (
+            ""
+          )}
+        </PostAuthorProfileContainer>
+      </PostSingleGrid>
     </Container>
   );
 }
